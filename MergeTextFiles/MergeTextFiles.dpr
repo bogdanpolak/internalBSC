22c632d3d0e24d365dc773e6b3d7bd735d272ebe
bogdanpolak <bogdan.polak@bsc.com.pl>
2018-09-27 15:52:10 +0200

    zmiana znazwy Form.Main.pas

--- MergeTextFiles/MergeTextFiles.dpr
+++ MergeTextFiles/MergeTextFiles.dpr
@@ -2,14 +2,15 @@ program MergeTextFiles;
 
 uses
   Vcl.Forms,
-  Form.Main in 'Form.Main.pas' {Form1},
-  Helper.TGroupBox in 'Helper.TGroupBox.pas';
+  Form.Main in 'Form.Main.pas' {FormMain},
+  Helper.TGroupBox in 'Helper.TGroupBox.pas',
+  Global.AppConfiguration in 'Global.AppConfiguration.pas';
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TFormMain, FormMain);
   Application.Run;
 end.
