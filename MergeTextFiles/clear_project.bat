a96d4d0c505d70fa4031965e1703cf9c0603a3bf
bogdanpolak <bogdan.polak@bsc.com.pl>
2018-09-27 17:26:05 +0200

    dodano clear_project.bat

---
+++ MergeTextFiles/clear_project.bat
@@ -0,0 +1,14 @@
+rmdir /Q /S __history
+rmdir /Q /S Win32
+rmdir /Q /S .svn
+rmdir /Q /S __recovery
+
+del *.identcache
+del *.dproj.local
+del desktop.ini
+del *.stat
+del *.tvsconfig
+
+del *.dcu
+del *.exe
+del *.dll
