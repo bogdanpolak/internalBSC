8531c779e1d863241316e8c3dc02cc5127056155
Bogdan <bogdan.polak@bsc.com.pl>
2019-01-07 15:35:42 +0100

    Rozszerzono OOP Demo o kontener DI

--- ObjectOrientedDemos/OOPDemos.dpr
+++ ObjectOrientedDemos/OOPDemos.dpr
@@ -9,12 +9,18 @@ uses
   Main.VirtualMetods in 'Main.VirtualMetods.pas',
   Main.GenericCollection in 'Main.GenericCollection.pas',
   DataModule.CenericCollection in 'DataModule.CenericCollection.pas' {DataModule1: TDataModule},
-  Helper.TDataSet in 'Helper.TDataSet.pas';
+  Helper.TDataSet in 'Helper.TDataSet.pas',
+  Main.DependencyInjectionContainer in 'Main.DependencyInjectionContainer.pas';
 
 begin
   try
+    // 1 --------------------------------------
     // Execute_VirtualMetodsDemo;
-    Execute_GenericCollectionDemo;
+    // 2 --------------------------------------
+    // Execute_GenericCollectionDemo;
+    // 3 --------------------------------------
+    Execute_DependencyInjectionContainerDemo;
+    // ----------------------------------------
     Write('+--> Press Enter to close ...');
     Readln;
   except
